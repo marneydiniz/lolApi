7210e7196a6dda669957f80df188d10f8841cb40
marneydiniz <86838602+marneydiniz@users.noreply.github.com>
2021-12-30 22:40:08 -0600

    Update list of matches

--- src/main/java/com/lolApi/lolApi/controllers/SummonerControler.java
+++ src/main/java/com/lolApi/lolApi/controllers/SummonerControler.java
@@ -1,17 +1,17 @@
 package com.lolApi.lolApi.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.lolApi.lolApi.models.Match;
-import com.lolApi.lolApi.models.Metadata;
 import com.lolApi.lolApi.models.Summoner;
 
 @RestController
@@ -21,17 +21,19 @@ public class SummonerControler {
 	@Value("${api.key}")
 	private String apiKey;
 	
-	@GetMapping(path = "/{name}")
-	public Summoner getSummonerByName(@PathVariable String name) {
+	@GetMapping
+	public Summoner getSummonerByName(@RequestParam(name = "name") String name) {
 		RestTemplate restTemplate = new RestTemplate();
 		
 		String summonerURI = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + apiKey;
 		Summoner summoner = restTemplate.getForObject(summonerURI, Summoner.class);
 		
 		if (summoner != null) {
-			List<Match> matches = getMatchesByPuuid(summoner.getPuuid());
-			summoner.setMatches(matches);
-			
+			List<Match> summonerMatchList = new ArrayList<Match>();
+			summoner.setMatches(summonerMatchList);
+			for (String match : getMatchesByPuuid(summoner.getPuuid())) {
+				summoner.getMatches().add(getMatchInfoById(match));
+			}
 			return summoner;
 		} else {
 			return null;
@@ -40,8 +42,8 @@ public class SummonerControler {
 	}
 	
 	@SuppressWarnings({ "unchecked" })
-	@GetMapping("/puuid/{puuid}")
-	public List<Match> getMatchesByPuuid (@PathVariable String puuid){
+	@GetMapping("/macthesbypuuid={puuid}")
+	public List<String> getMatchesByPuuid (@PathVariable String puuid){
 		
 		RestTemplate restTemplate = new RestTemplate();
 		String matchesURI = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=20&api_key=" + apiKey;
@@ -49,13 +51,22 @@ public class SummonerControler {
 		return restTemplate.getForObject(matchesURI, java.util.List.class);
 	}
 	
-	@GetMapping("/match/{match-id}")
-	public ResponseEntity<Match> getMatchInfoById (@PathVariable Metadata matchId) {
+	@GetMapping("/matchinfobyid={matchId}")
+	public Match getMatchInfoById (@PathVariable String matchId) {
 		
 		RestTemplate restTemplate = new RestTemplate();
-		String matcheURI = "https://americas.api.riotgames.com/lol/match/v5/matches/" + matchId + "?api_key=" + apiKey;
+		String matcheInfoURI = "https://americas.api.riotgames.com/lol/match/v5/matches/" + matchId + "?api_key=" + apiKey;
 		
-		return restTemplate.getForEntity(matcheURI, Match.class);
+		return restTemplate.getForObject(matcheInfoURI, Match.class);
+	}
+	
+	@GetMapping("/summonerbypuuid={puuid}")
+	public Summoner getSummonerInfoByPuuid (@PathVariable String puuid) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String summonerInfoURI = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + puuid + "?api_key=" + apiKey;
+		
+		return restTemplate.getForObject(summonerInfoURI, Summoner.class);
 	}
 	
 }
